334c9ce2de8c125abce7d37d2733aac028cdafb8
ankita-2021 <ankitachakrawarti@gmail.com>
2025-07-15 11:08:48 +0530

    intial commit

---
+++ lesson38/activity3.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Education"
+full_name = first_name + " " + last_name
+example = "Haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times gives this result:", example)
+
+word = "Codingal"
+print("Length of string :", len(word))
+print("First letter of String: ", word[0])
+print("Last letter of String: ", word[5])
+print("String Sliced:", word[0:3])

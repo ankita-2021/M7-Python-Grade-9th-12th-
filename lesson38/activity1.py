0045aae42525ff9e9a5fcfe5fc62dd81839be5ec
ankita-2021 <ankitachakrawarti@gmail.com>
2025-07-15 10:49:20 +0530

    inital commit

---
+++ lesson38/activity1.py
@@ -0,0 +1,21 @@
+name ="Ankita"
+age = 25
+is_teacher = True
+weight = 56.5
+
+print("Name :", name)
+print("Data Type of Name is :", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_teacher)
+print("Data Type of is_student is", type(is_teacher))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
